795ad993acda530b570c614758493e16846d1814
Yisak Abraham <yisakabraham10@gmail.com>
2026-09-21 16:57:05 +0300

    fix: allow production frontend origin

--- src/main.py
+++ src/main.py
@@ -24,6 +24,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://trusted-information-intelligence.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
