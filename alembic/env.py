5c0998517d33bfc929ccaab4f7bb5340f0394964
Yisak Abraham <yisakabraham10@gmail.com>
2026-09-21 15:02:56 +0300

    fix: use psycopg3 for alembic migrations

--- alembic/env.py
+++ alembic/env.py
@@ -11,7 +11,16 @@ from alembic import context
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", settings.database_url)
+database_url = settings.database_url
+
+if database_url.startswith("postgresql://"):
+    database_url = database_url.replace(
+        "postgresql://",
+        "postgresql+psycopg://",
+        1,
+    )
+
+config.set_main_option("sqlalchemy.url", database_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
